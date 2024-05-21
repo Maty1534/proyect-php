acb8bfd415c88db85ac3b535ccbfeaee91c1c2a3
Correa Matias Emanuel <correamatias.0922@gmail.com>
2024-05-20 23:11:23 -0300

    quedo. login hecho lo malo es la optimizacion del js (eliminar)

--- Src/Js/scripts.js
+++ Src/Js/scripts.js
@@ -1,1 +1,12 @@
-import "./../Styles/styles.css";
+import "../Styles/styles.css";
+
+const togglePasswordBtn = document.getElementById("togglePassword");
+const passwordInput = document.getElementById("password");
+const eyeIcon = document.getElementById("eye-icon");
+
+togglePasswordBtn.addEventListener("click", function () {
+  const type =
+    passwordInput.getAttribute("type") === "password" ? "text" : "password";
+  passwordInput.setAttribute("type", type);
+  eyeIcon.classList.toggle("eye-closed");
+});
